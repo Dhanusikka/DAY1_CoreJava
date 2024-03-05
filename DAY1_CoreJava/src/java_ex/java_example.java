92d8d67e674dfb2aab47d400d241fc5ffbded104
dhanu <dhanu@Dhanu>
2024-03-05 16:33:12 +0530

    additional changes

--- DAY1_CoreJava/src/java_ex/java_example.java
+++ DAY1_CoreJava/src/java_ex/java_example.java
@@ -6,13 +6,15 @@ public class java_example {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  Scanner s=new Scanner(System.in);
- int a,b,c;
+ int a,b,c,d;
  System.out.println("Enter values for a and b: ");
  a=s.nextInt();
  b=s.nextInt();
  
  c=a+b;
+ d=a-b;
  System.out.println("Addition of a="+a+" and b="+b+" is ="+c);
+ System.out.println("Subtraction of a="+a+" and b="+b+" is ="+d);
 	}
 
 }
