05461f6c62032021c85cb63ff256884b5123cf7e
dhanu <dhanu@Dhanu>
2024-03-05 15:45:12 +0530

    This is my Java first project.

---
+++ DAY1_CoreJava/src/java_ex/java_example.java
@@ -0,0 +1,18 @@
+package java_ex;
+import java.util.Scanner;
+
+public class java_example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ Scanner s=new Scanner(System.in);
+ int a,b,c;
+ System.out.println("Enter values for a and b: ");
+ a=s.nextInt();
+ b=s.nextInt();
+ 
+ c=a+b;
+ System.out.println("Addition of a="+a+" and b="+b+" is ="+c);
+	}
+
+}
---
+++ DAY1_CoreJava/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module DAY1_CoreJava {
+}
