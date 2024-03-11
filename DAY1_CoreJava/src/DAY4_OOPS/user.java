8eda3ed889c3e7c4a8eb2f0c1b7d3386394a9169
dhanu <dhanu@Dhanu>
2024-03-11 15:42:40 +0530

    Day 4

---
+++ DAY1_CoreJava/src/DAY4_OOPS/user.java
@@ -0,0 +1,13 @@
+package DAY4_OOPS;
+
+import java.util.Scanner;
+
+public class user{
+	public static void main(String args[]) {
+		Scanner input= new Scanner(System.in);
+		System.out.println("Enter your name: ");
+		String n= input.nextLine();
+		System.out.println("Hello " + n);
+	}
+
+}
